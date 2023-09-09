576355b997f945fd39917afe3865285848d3a27b
Joshimello <joshualeanjw@gmail.com>
2023-09-09 20:19:54 +0800

    uwu

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,115 @@
+<script>
+  import 'virtual:windi.css'
+  import { tweened } from 'svelte/motion'
+  import { fly } from 'svelte/transition'
+  import { interpolateLab } from 'd3-interpolate'
+  import logo from '$lib/img/logo1.png'
+
+  const bgColor = tweened('#ffffff00', {
+    interpolate: interpolateLab,
+    duration: 100
+  })
+
+  const textColor = tweened('#ffffffff', {
+    interpolate: interpolateLab,
+    duration: 100
+  })
+
+  let y = 0, w = 0
+  $: if(y == 0){
+    bgColor.set('#ffffff00')
+    textColor.set('#ffffffff')
+  } else {
+    bgColor.set('#ffffffff')
+    textColor.set('#000000')
+  }
+
+  let open = false
+
+</script>
+
+<svelte:head>
+  <title>NTURPG</title> 
+</svelte:head>
+
+<svelte:window bind:scrollY={y} bind:innerWidth={w} />
+
+<div class="fixed w-full flex px-64 <xl:px-32 <lg:px-4 py-2 shadow-lg" style:background-color={$bgColor}>
+
+  <div class="flex items-center gap-8 flex-1">
+    <img class="h-16" src={logo} alt="" />
+    <span class="text-2xl" style:color={$textColor}>NTU RPG</span>
+  </div>
+  
+  <div class="flex items-center gap-8">
+    {#if w > 639}
+    <a class="" href="#1">
+      <span class="text-xl underline-offset-6 hover:underline" style:color={$textColor}>
+        主頁
+      </span>
+    </a>
+    <a class="" href="#2">
+      <span class="text-xl underline-offset-6 hover:underline" style:color={$textColor}>
+        關於
+      </span>
+    </a>
+    <a class="" href="#3">
+      <span class="text-xl underline-offset-6 hover:underline" style:color={$textColor}>
+        介紹
+      </span>
+    </a>
+    <span style:color={$textColor}>|</span>
+    <a class="" href="#4">
+      <span class="text-xl underline-offset-6 hover:underline" style:color={$textColor}>
+        Discord
+      </span>
+    </a>
+    {:else}
+    <button on:click={()=>{open = true}}>
+      <span class="text-2xl" style:color={$textColor}>
+        ☰
+      </span>
+    </button>
+    {/if}
+  </div>
+  
+</div>
+
+{#if open}
+<div class="fixed w-full bg-white flex flex-col items-end gap-4 py-6 px-5" transition:fly={{ y: -50 }}>
+  <button on:click={()=>{open = false}} class="w-max">
+    <span class="text-2xl">
+      X
+    </span>
+  </button>
+  <a class="" href="#1">
+    <span class="text-xl underline-offset-6 hover:underline">
+      主頁
+    </span>
+  </a>
+  <a class="" href="#2">
+    <span class="text-xl underline-offset-6 hover:underline">
+      關於
+    </span>
+  </a>
+  <a class="" href="#3">
+    <span class="text-xl underline-offset-6 hover:underline">
+      介紹
+    </span>
+  </a>
+  <span>—</span>
+  <a class="" href="#4">
+    <span class="text-xl underline-offset-6 hover:underline">
+      Discord
+    </span>
+  </a>
+</div>
+{/if}
+
+<slot/>
+
+<style>
+  :global(html) {
+    scroll-behavior: smooth;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,111 @@
+<script>
+  import img1 from '$lib/img/1.png'
+  import img2 from '$lib/img/2.png'
+  import logo2 from '$lib/img/logo2.png'
+  import img3 from '$lib/img/3.png'
+  import img4 from '$lib/img/4.png'
+  import img5 from '$lib/img/5.png'
+  import img6 from '$lib/img/6.png'
+</script>
+
+<div class="w-full h-100vh" id="1">
+  <img class="w-full h-full object-cover absolute -z-1" src={img1} alt=""/>
+  <div class="w-full h-full flex flex-col px-64 py-50 <xl:px-32 <sm:px-8">
+    <div class="flex flex-col gap-4 flex-1">
+      <span class="text-white text-4xl">
+        NTU RPG
+      </span>
+      <span class="text-white text-lg">
+        一個休閑的好地方~
+      </span>
+    </div>
+    <div class="flex items-end justify-end">
+      <button class="text-white border-2 px-8 py-4 rounded-full <sm:w-full">
+        <span class="text-white text-2xl">
+          了解更多
+        <span/>
+      </button>
+    </div>
+  </div>
+</div>
+
+<div class="w-full flex gap-8 px-64 <xl:px-32 <lg:px-4 py-16 <sm:flex-col" id="2">
+  <div class="flex-1">
+    <img class="" src={img2} alt=""/>
+  </div>
+  <div class="flex-1 flex flex-col gap-4 items-center justify-center">
+    <span class="text-4xl">
+      關於
+    </span>
+    <span class="text-lg text-center">
+      你從Minecraft世界打破了世界邊界，來到了異世界！<br>
+      你將在異世界冒險，和跟著故事主綫一起<br>
+      體驗劇情和欣賞風景！<br>
+    </span>
+  </div>
+</div>
+
+<div class="w-full flex flex-col items-center justify-center p-16 border-y-3" id="3">
+  <img src={logo2} alt=""/>
+  <span class="text-4xl pt-6">
+    認識 NTURPG
+  </span>
+  <span class="text-lg pt-2">
+    歡迎來到RPG世界！
+  </span>
+</div>
+
+<div class="w-full flex flex-col items-center justify-center p-16 border-b-3">
+  <span class="text-4xl pt-6">
+    IP: nturpg.xyz
+  </span>
+  <span class="text-lg pt-2">
+    版本：1.20.1
+  </span>
+</div>
+
+<div class="w-full flex flex-col gap-16 px-64 <xl:px-32 <lg:px-4 py-16 <sm:flex-col">
+  <div class="w-full flex items-center justify-center">
+    <span class="text-4xl">
+      伺服器展示
+    </span>
+  </div>
+  <div class="grid grid-cols-2 gap-8 <sm:grid-cols-1">
+    <div class="flex flex-col items-center gap-2">
+      <img src={img3} alt=""/>
+      <span class="text-xl">
+        副本小鎮
+      </span>
+    </div>
+    <div class="flex flex-col items-center gap-2">
+      <img src={img4} alt=""/>
+      <span class="text-xl">
+        精靈古樹
+      </span>
+    </div>
+    <div class="flex flex-col items-center gap-2">
+      <img src={img5} alt=""/>
+      <span class="text-xl">
+        月宮
+      </span>
+    </div>
+    <div class="flex flex-col items-center gap-2">
+      <img src={img6} alt=""/>
+      <span class="text-xl">
+        大和島國
+      </span>
+    </div>
+  </div>
+</div>
+
+<div class="w-full flex flex-col gap-4 items-center justify-center p-16 border-y-3" id="4">
+  <span class="text-4xl">
+    加入我們 Discord!
+  </span>
+  <span class="text-lg">
+    Discord: <a class="underline" target="_blank" href="https://discord.gg/FzmF6ZBxkb">https://discord.gg/FzmF6ZBxkb</a>
+  </span>
+  <iframe src="https://discord.com/widget?id=1081818193158479892&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+</div>
+
+<div class="w-full h-50vh"></div>
